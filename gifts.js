bd9a80012582d33fc48521926c0b7813f87d47c2
Twonki <Leonhard.Applis@Protonmail.com>
2020-12-21 15:20:06 +0100

    Added some gifts, and placeholder instead of value

--- gifts.js
+++ gifts.js
@@ -1,5 +1,7 @@
 
 var gifts = {
     "Encrypted-Test":{"ct":"VPPw5gZHvBlmyYQupCzAVQ==","iv":"30bca5b956fe9ef987c17bc5192e98fa","s":"a1e522b3a86b1f32"},
-    "Sexy-Kevin-2020":{"ct":"gLFl23uTDm390QWL452zReo565TxHTAjA/rr4ZbV5OU=","iv":"7c25ff040996d97a82b38408fe01fc91","s":"2b5d90672d8a0f46"}
+    "Sexy-Kevin-2020":{"ct":"gLFl23uTDm390QWL452zReo565TxHTAjA/rr4ZbV5OU=","iv":"7c25ff040996d97a82b38408fe01fc91","s":"2b5d90672d8a0f46"},
+    "Flo-Secret-Santa-2021":{"ct":"cYuiiZhGDEKpHZl0eQzfUBNrvCbw2PndnztahiHcY+1F0rPa5WKTtkXn+ielTFytgJr2PlVFS50D+BXCgQmfRA==","iv":"15b3613e0ae108497377e4afa1811af4","s":"5ffd366e76c4d82d"},
+    "Momo-Happy-2020": {"ct":"EWBPXlePas/xJW+7FFeUTI7zpUVLgUKpRsylwx6ECLLeBPBy/2UOdmkBj7XOEBqNjn4fB3Dco4SXvu5Lm3zTOQ==","iv":"553a9b1b74d6ca7b6c41b06bdc6bd8c0","s":"afc44d526ae9f139"}
 };
