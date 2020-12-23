fc82c6ecc57b18d23725d0dd674e4bb0513acbbc
Twonki <Leonhard.Applis@Protonmail.com>
2020-12-23 10:50:58 +0100

    Added some Gifts

--- gifts.js
+++ gifts.js
@@ -4,5 +4,10 @@ var gifts = {
     "Sexy-Kevin-2020":{"ct":"gLFl23uTDm390QWL452zReo565TxHTAjA/rr4ZbV5OU=","iv":"7c25ff040996d97a82b38408fe01fc91","s":"2b5d90672d8a0f46"},
     "Flo-Secret-Santa-2021":{"ct":"cYuiiZhGDEKpHZl0eQzfUBNrvCbw2PndnztahiHcY+1F0rPa5WKTtkXn+ielTFytgJr2PlVFS50D+BXCgQmfRA==","iv":"15b3613e0ae108497377e4afa1811af4","s":"5ffd366e76c4d82d"},
     "Momo-Happy-2020": {"ct":"EWBPXlePas/xJW+7FFeUTI7zpUVLgUKpRsylwx6ECLLeBPBy/2UOdmkBj7XOEBqNjn4fB3Dco4SXvu5Lm3zTOQ==","iv":"553a9b1b74d6ca7b6c41b06bdc6bd8c0","s":"afc44d526ae9f139"},
-    "CA-RO-XMAS-2020":{"ct":"7020AZbVogWdi9IDnWA38nT7vGowEnC8XILQ08HBE7XqdbJNsMHRI1Sex5CnJbfH","iv":"b47c7dd30d3959891eafe8a90dec3549","s":"aff9d7853ea9c0bf"}
+    "CA-RO-XMAS-2020":{"ct":"7020AZbVogWdi9IDnWA38nT7vGowEnC8XILQ08HBE7XqdbJNsMHRI1Sex5CnJbfH","iv":"b47c7dd30d3959891eafe8a90dec3549","s":"aff9d7853ea9c0bf"},
+    "PNX-UUV2-ZF1-PP4":{"ct":"O3UU2N2DDqzf/C0gMmyL9Qv4pPQt+RZSIkJ2MaCAfhXY1Ht4Hf6oe6Dc7zdmuTRl","iv":"91495b521bc7dc86ab11643d042d2ed3","s":"5b7469894fda0915"},
+    "KKV-N241-JUR-NGA":{"ct":"FBtL5DijE1mtHSHiGDuB02mCiAS4ZIzSly9hBn0K4vKxxkk4+4MrekLbdMOFtcR6DOEENkT3maE/deA/dAhM+A==","iv":"9dcea84f2a34a991fa153542625fd420","s":"0cbc6a8979f0ff49"},
+    "LGP-42NH-KL2-Q31":{"ct":"qhL+Ho6Qf+gBoMry3xsALzE8AJjBpLkARh3oaVBptbhylQBcjHmJW9n7SiqEi4Yf","iv":"f1a0864917443d043cd4b82938c8c808","s":"6122b154d5004d6f"},
+    "KLZ-XT9E-UP1-HM2":{"ct":"gmV8aXcvkSsjIOqq6nZZQ1f1XmfQLBbGVhRSN43Lzfsui6EQRgTBOjECNVffRXQT","iv":"27ad627a5e6bb28e689ff8120ff40ea5","s":"f0dd4842609a4545"},
+    "LR4-KM44-HE6-PCY":{"ct":"FOXPpMdX055ybUHUoEHyRCYXDABi6sCcL9l68VK6NX0=","iv":"af02a7aaa0056f62d028de94652a5919","s":"7b7f1bbd9160ae49"}
 };
